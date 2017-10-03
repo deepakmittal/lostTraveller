15ea5f15ed68b64330bc854f1713944763d0366a
Deepak Mittal <deepak@vdopia.com>
2017-10-03 12:01:51 +0530

    pilot

---
+++ js/places.js
@@ -0,0 +1,277 @@
+PLACES = [
+  {
+    "code": 1,
+    "city": "Melbourne",
+    "country": "Australia",
+    "latitude": -37.78333333,
+    "lognitude": -144.58
+  },
+  {
+    "code": 2,
+    "city": "Perth",
+    "country": "Australia",
+    "latitude": -31.95,
+    "lognitude": -115.52
+  },
+  {
+    "code": 3,
+    "city": "Sydney",
+    "country": "Australia",
+    "latitude": -34,
+    "lognitude": -151
+  },
+  {
+    "code": 4,
+    "city": "Brussels",
+    "country": "Belgium",
+    "latitude": 50.86666667,
+    "lognitude": -4.22
+  },
+  {
+    "code": 5,
+    "city": "Rio de Janeiro",
+    "country": "Brazil",
+    "latitude": -22.95,
+    "lognitude": 43.12
+  },
+  {
+    "code": 6,
+    "city": "Beijing",
+    "country": "China",
+    "latitude": 39.91666667,
+    "lognitude": -116.25
+  },
+  {
+    "code": 7,
+    "city": "Hong Kong",
+    "country": "China",
+    "latitude": 22.33333333,
+    "lognitude": -114.11
+  },
+  {
+    "code": 8,
+    "city": "Shanghai",
+    "country": "China",
+    "latitude": 31.16666667,
+    "lognitude": -121.28
+  },
+  {
+    "code": 9,
+    "city": "Prague",
+    "country": "Czech Republic",
+    "latitude": 50.08333333,
+    "lognitude": -14.26
+  },
+  {
+    "code": 10,
+    "city": "Cairo",
+    "country": "Egypt",
+    "latitude": 30.03333333,
+    "lognitude": -31.21
+  },
+  {
+    "code": 11,
+    "city": "London",
+    "country": "England",
+    "latitude": 51.53333333,
+    "lognitude": 0.05
+  },
+  {
+    "code": 12,
+    "city": "Paris",
+    "country": "France",
+    "latitude": 48.8,
+    "lognitude": -2.2
+  },
+  {
+    "code": 13,
+    "city": "Berlin",
+    "country": "Germany",
+    "latitude": 52.5,
+    "lognitude": -13.25
+  },
+  {
+    "code": 14,
+    "city": "Athens",
+    "country": "Greece",
+    "latitude": 37.96666667,
+    "lognitude": -23.43
+  },
+  {
+    "code": 15,
+    "city": "Reykjavík",
+    "country": "Iceland",
+    "latitude": 64.06666667,
+    "lognitude": 21.58
+  },
+  {
+    "code": 16,
+    "city": "Bombay",
+    "country": "India",
+    "latitude": 19,
+    "lognitude": -72.48
+  },
+  {
+    "code": 17,
+    "city": "New Delhi",
+    "country": "India",
+    "latitude": 28.58333333,
+    "lognitude": -77.12
+  },
+  {
+    "code": 18,
+    "city": "Jakarta",
+    "country": "Indonesia",
+    "latitude": -6.266666667,
+    "lognitude": -106.48
+  },
+  {
+    "code": 19,
+    "city": "Rome",
+    "country": "Italy",
+    "latitude": 41.9,
+    "lognitude": -12.27
+  },
+  {
+    "code": 20,
+    "city": "Venice",
+    "country": "Italy",
+    "latitude": 45.43333333,
+    "lognitude": -12.2
+  },
+  {
+    "code": 21,
+    "city": "Tokyo",
+    "country": "Japan",
+    "latitude": 35.66666667,
+    "lognitude": -139.45
+  },
+  {
+    "code": 22,
+    "city": "Nairobi",
+    "country": "Kenya",
+    "latitude": -1.416666667,
+    "lognitude": -36.55
+  },
+  {
+    "code": 23,
+    "city": "Tananarive",
+    "country": "Madagascar",
+    "latitude": -18.83333333,
+    "lognitude": -47.33
+  },
+  {
+    "code": 24,
+    "city": "Kuala Lumpur",
+    "country": "Malaysia",
+    "latitude": 3.133333333,
+    "lognitude": -101.42
+  },
+  {
+    "code": 25,
+    "city": "Mexico City",
+    "country": "Mexico",
+    "latitude": 19.43333333,
+    "lognitude": 99.07
+  },
+  {
+    "code": 26,
+    "city": "Rangoon",
+    "country": "Myanmar",
+    "latitude": 16.83333333,
+    "lognitude": -96
+  },
+  {
+    "code": 27,
+    "city": "Amsterdam",
+    "country": "Netherlands",
+    "latitude": 52.36666667,
+    "lognitude": -4.53
+  },
+  {
+    "code": 28,
+    "city": "Auckland",
+    "country": "New Zealand",
+    "latitude": -36.86666667,
+    "lognitude": -174.45
+  },
+  {
+    "code": 29,
+    "city": "Panama City",
+    "country": "Panama",
+    "latitude": 8.966666667,
+    "lognitude": 79.32
+  },
+  {
+    "code": 30,
+    "city": "Lisbon",
+    "country": "Portugal",
+    "latitude": 38.73333333,
+    "lognitude": 9.09
+  },
+  {
+    "code": 31,
+    "city": "Moscow",
+    "country": "Russia",
+    "latitude": 55.75,
+    "lognitude": -37.36
+  },
+  {
+    "code": 32,
+    "city": "Mecca",
+    "country": "Saudi Arabia",
+    "latitude": 21.48333333,
+    "lognitude": -39.45
+  },
+  {
+    "code": 33,
+    "city": "Singapore",
+    "country": "Singapore",
+    "latitude": 1.233333333,
+    "lognitude": -103.55
+  },
+  {
+    "code": 34,
+    "city": "Cape Town",
+    "country": "South Africa",
+    "latitude": -33.91666667,
+    "lognitude": -18.22
+  },
+  {
+    "code": 35,
+    "city": "Johannesburg",
+    "country": "South Africa",
+    "latitude": -26.2,
+    "lognitude": -28.04
+  },
+  {
+    "code": 36,
+    "city": "Barcelona",
+    "country": "Spain",
+    "latitude": 41.38333333,
+    "lognitude": -2.09
+  },
+  {
+    "code": 37,
+    "city": "Stockholm",
+    "country": "Sweden",
+    "latitude": 59.28333333,
+    "lognitude": -18.03
+  },
+  {
+    "code": 38,
+    "city": "Zürich",
+    "country": "Switzerland",
+    "latitude": 47.35,
+    "lognitude": -8.31
+  },
+  {
+    "code": 39,
+    "city": "Bangkok",
+    "country": "Thailand",
+    "latitude": 13.75,
+    "lognitude": -100.3
+  }
+];
+
+
